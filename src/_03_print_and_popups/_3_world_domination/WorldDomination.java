44f57db1d3947a82d7c800c8b29961f396e26f60
root <root@442d950f1a8a>
2020-05-30 21:32:45 +0000

    working on scary maze

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -5,14 +5,21 @@ import javax.swing.JOptionPane;
 
 public class WorldDomination {
 	
+
+
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+	String question = JOptionPane.showInputDialog("Do you know how to write code?");
+	System.out.println(question);
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(question.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You will rule the world!");
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+		}
+		else if (question.equals("no")) {
+			JOptionPane.showMessageDialog(null, "Then learn some code.");
+		}
 	}
 }
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,45 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+		String question = JOptionPane.showInputDialog("How many planets are there in our Solar System?");
+		System.out.println(question);
 				// 3.  Use an if statement to check if their answer is correct
-				
+		if(question.equals("8")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score ++;
+		}
+		else {
+			score --;
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
+		question = JOptionPane.showInputDialog("Where did camels originate from?");
+		System.out.println(question);
+		if(question.equals("North America")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score ++;
+		}
+		else {
+			score --;
+			JOptionPane.showMessageDialog(null, "Incorrect");
+		}
+		System.out.println(score);
 		
 		
+		}
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
-}
+
